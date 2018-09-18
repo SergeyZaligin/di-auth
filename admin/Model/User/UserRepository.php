eb183f114199ca7bf3a2522e011079eed7958b59
sergey <sergey>
2018-09-18 15:27:28 +0300

    fix

--- admin/Model/User/UserRepository.php
+++ admin/Model/User/UserRepository.php
@@ -2,7 +2,8 @@
 
 namespace Admin\Model\User;
 use Engine\Model;
-use Engine\DI\DI;
+
+
 /**
  * Description of UserRepository
  *
@@ -10,9 +11,7 @@ use Engine\DI\DI;
  */
 class UserRepository extends Model
 {
-    public function __construct(DI $di) {
-        parent::__construct($di);
-    }
+    
     public function getAll() 
     {
         return $this->db->query('SELECT * FROM user ORDER BY id DESC', []);
